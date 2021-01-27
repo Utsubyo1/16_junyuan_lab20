1aec5435d3988307d6ae55bb0ef405c7b6df5b88
utsubyo <72735425+ustubyo@users.noreply.github.com>
2021-01-27 16:01:42 +0800

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Playergo;
+    Vector3 posOffset = new Vector3(0, 8.0f, -9.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, Playergo.transform.position + posOffset, 0.1f);
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    float movespeed  = 5f;
+    float rotatespeed = 1.0f;
+    float jumpforce = 5f;
+
+    int maxspeed = 5;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * movespeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * movespeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -rotatespeed, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotatespeed, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
+    }
+    
+}
